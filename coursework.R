0d653cf459841eb3008236a52a656eafe8c3927a
Divya Shridar <56643379+dibsondivya@users.noreply.github.com>
2023-03-22 07:09:25 +0000

    Add files via upload

---
+++ coursework.R
@@ -0,0 +1,26 @@
+install.packages("RMariaDB") # to be run only once if yet to be installed
+
+# Initialise imported package
+library(DBI)
+
+# Connect to the MySQL database
+con <- dbConnect(RMariaDB::MariaDB(), 
+                 dbname = "London_Paris_DB", 
+                 host = "localhost", 
+                 port = 3306,
+                 user = "root",
+                 password = "student086")
+
+# Get table names
+tables <- dbListTables(con)
+
+# Display structure of tables
+str(tables) # showed four table names of "country_ids",
+# "sample_char_genotypes", "sample_characteristics" and "sample_genotypes"
+
+# While still connected, import data
+sample_genotypes <- dbReadTable(con, "sample_genotypes")
+head(sample_genotypes)
+
+# Always cleanup by disconnecting the database
+dbDisconnect(con)
